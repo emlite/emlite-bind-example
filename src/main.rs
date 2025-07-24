7a06d5a9cf55995208778c4c205fb315cefb0e8a
Mohammed Alyousef <may642_2000@hotmail.com>
2025-07-24 11:44:01 +0300

    update example

--- src/main.rs
+++ src/main.rs
@@ -7,26 +7,26 @@ use webbind::window;
 fn main() {
     let con = Console::get();
     let document = window().document();
-    let bodies = document.get_elements_by_tag_name("body");
+    let bodies = document.get_elements_by_tag_name(&"body".into());
     if bodies.length() == 0 {
         con.log(&["I Ain't got Nobody!".into()]);
         return;
     }
     let body = bodies.item(0);
     let mut button = document
-        .create_element0("BUTTON")
+        .create_element0(&"BUTTON".into())
         .dyn_into::<HTMLButtonElement>()
         .unwrap();
 
     let style = button.style();
-    style.set_property0("color", "red");
-    style.set_property0("background-color", "#aaf");
-    style.set_property0("border", "solid");
+    style.set_property0(&"color".into(), &"red".into());
+    style.set_property0(&"background-color".into(), &"#aaf".into());
+    style.set_property0(&"border".into(), &"solid".into());
 
-    button.set_text_content("Click me");
+    button.set_text_content(&"Click me".into());
     println!("{}", button.text_content());
     button.add_event_listener0(
-        "click",
+        &"click".into(),
         &Closure::bind1(move |p: PointerEvent| {
             con.log(&[format!("You clicked at x:{}, y:{}\n", p.client_x(), p.client_y()).into()]);
         }).into()
