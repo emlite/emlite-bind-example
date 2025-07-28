1792bd8ec0d2bd8fc38f5d165998c519af074010
Mohammed Alyousef <may642_2000@hotmail.com>
2025-07-28 11:02:30 +0300

    build for release

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ async function main() {
     ];
     let wasi = new WASI([], [], fds);
     const emlite = new Emlite();
-    const bytes = await emlite.readFile(new URL("../target/wasm32-wasip1/debug/emlite-bind-example.wasm", import.meta.url));
+    const bytes = await emlite.readFile(new URL("../target/wasm32-wasip1/release/emlite-bind-example.wasm", import.meta.url));
     let wasm = await WebAssembly.compile(bytes);
     let inst = await WebAssembly.instantiate(wasm, {
         "wasi_snapshot_preview1": wasi.wasiImport,
